05ecac7b155f8d636781ab55f1a05327b2f1ba14
AresWg <gord0213@algonquinlive.com>
2016-09-14 18:53:38 -0400

    Added 3 activies for Lab 2

---
+++ app/src/main/java/com/example/mike/lab1/ListItemsActivity.java
@@ -0,0 +1,15 @@
+package com.example.mike.lab1;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.GridLayout;
+
+public class ListItemsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_list_items);//GridLayout);//
+
+    }
+}
